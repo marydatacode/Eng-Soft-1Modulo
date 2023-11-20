680b381568a7a1ba1cd8f7333f753905e76b2fe8
Eduarda Marques <150185136+marydatacode@users.noreply.github.com>
2023-11-20 20:26:46 -0300

    For

---
+++ Estrutura For.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+      
+      for (int cont=5; cont <= 20; cont++){ //escopo do for
+        System.out.println("Números de 5 a 20 " + cont); //escopo do for
+      }
+    }
+  }
