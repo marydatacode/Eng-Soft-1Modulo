d5ddb49586aa1b0696a51d17259729aab9fd16dc
Eduarda Marques <150185136+marydatacode@users.noreply.github.com>
2023-11-30 17:23:41 -0300

    Atividade Herança

---
+++ Cachorro.java
@@ -0,0 +1,5 @@
+class Cachorro extends Animal {
+  public void mostrar(){
+    System.out.println("Eu sou um cachorro e meu nome é: " + getNome());
+  }
+}
---
+++ Gato.java
@@ -0,0 +1,5 @@
+class Gato extends Animal {
+  public void mostrar(){ //polimorfismo é o mesmo método para ações diferentes, distintas
+    System.out.println("E eu sou um gato e meu nome é: " + getNome());
+  }
+}
