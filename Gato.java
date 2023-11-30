c6eda9b07bc0d8cb23bc6565cf48fd935168fa0e
Eduarda Marques <150185136+marydatacode@users.noreply.github.com>
2023-11-30 17:34:24 -0300

    Delete Gato.java

--- Gato.java
+++
@@ -1,5 +0,0 @@
-class Gato extends Animal {
-  public void mostrar(){ //polimorfismo é o mesmo método para ações diferentes, distintas
-    System.out.println("E eu sou um gato e meu nome é: " + getNome());
-  }
-}
