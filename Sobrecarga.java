8aba4d777e938e5b2cbf9dd01f3fba06a8400e15
Eduarda Marques <150185136+marydatacode@users.noreply.github.com>
2023-12-01 14:46:21 -0300

    Polimorfismo - Sobrecarga de Métodos

---
+++ Sobrecarga.java
@@ -0,0 +1,36 @@
+class Desenho {
+    // método sem paramentro
+    public void mostrar(){
+      for(int i=0; i<10; i++){
+        System.out.println("*");
+      }
+    } 
+
+  // método com um parametro
+  public void mostrar(char simb){
+    for(int i=0; i<10; i++){
+      System.out.println(simb);
+    }
+  }
+
+  // método com dois parametros
+  public void mostrar(char simb, int n){
+    for(int i=0; i<n; i++){
+      System.out.println(simb);
+    }
+  }
+} 
+
+
+
+class Main {
+  public static void main(String[] args) {
+    Desenho d1 = new Desenho();
+    
+    d1.mostrar();
+    
+    d1.mostrar('#');
+
+    d1.mostrar('$', 3);
+  }
+}
