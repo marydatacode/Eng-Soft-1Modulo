3742569687afc5823378d7d52710777efe99e6d4
Eduarda Marques <150185136+marydatacode@users.noreply.github.com>
2023-12-02 20:13:11 -0300

    Colleci

---
+++ LinkedList.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+class Main {
+  public static void main(String[] args) {
+    LinkedList<String> animais = new LinkedList<>();
+
+    animais.add("Cachorro");
+    animais.add("Cavalo");
+    animais.add("Vaca");
+    System.out.println("LinkedList: " + animais);
+
+    animais.add(1, "Gato");
+    System.out.println("LinkedList: " + animais);
+  }
+}
